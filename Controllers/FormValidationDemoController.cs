1782631547a8fe2f7f74276adcedfb81150c67f1
yyoungeun <1150amy@naver.com>
2020-03-26 14:40:52 +0900

    폼 유효성 검사_0326

---
+++ Controllers/FormValidationDemoController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DotNetNote.Models;
+
+namespace DotNetNote.Controllers
+{
+    public class FormValidationDemoController : Controller
+    {
+        //[1] 폼 유효성 검사 테스트용 메인 페이지 작성
+        #region Main Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        //[2] 순수HTML과 JavaScript를 사용한 유효성 검사
+        //[HttpGet] 생략
+        #region HTML
+        public IActionResult Html()
+        {
+            return View();
+        }
+
+        //[HttpPost] 생략
+        public IActionResult HtmlProcess(string txtName, string txtContent)
+        {
+            ViewBag.ResultString = $"이름: {txtName}, 내용: {Request.Form["txtContent"]}";
+            return View();
+        }
+        #endregion
+
+
+        #region Helper Method
+        [HttpGet]
+        public IActionResult HelperMethod()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult HelperMethod(string txtName, string txtContent)
+        {
+            ViewBag.ResultString = $"이름: {txtName}, 내용: {txtContent}";
+            return View();
+        }
+        #endregion
+
+        #region Strongly Type View + Model Binding
+        public IActionResult StronglyTypeView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult StronglyTypeView(MaximModel model)
+        {
+            return View();
+        }
+        #endregion
+
+        #region Model Validation + Server Valdation
+        public IActionResult ModelValidation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ModelValidation(MaximModel model)
+        {
+            //직접 유효성 검사
+            if (string.IsNullOrEmpty(model.Name))
+            {
+                ModelState.AddModelError("Name", "이름을 입력하세요.");
+            }
+            if (string.IsNullOrEmpty(model.Content))
+            {
+                ModelState.AddModelError("content", "내용을 입력하세요.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                //@Html.ValidationSummary(true)일 때는 아래 에러만 표시
+                ModelState.AddModelError("", "모든 에러");
+            }
+
+            //넘어온 모델에 대한 유효성 검사
+            if (ModelState.IsValid)  //정확한 데이터
+            {
+                return View("completed");
+            }
+            return View();
+        }
+
+        public IActionResult Completed()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ Models/MaximModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetNote.Models
+{
+    public class MaximModel
+    {
+        public int Id { get; set; }
+        
+        [Display(Name ="이름")] //자동으로 생성되는 레이블 표시
+        [Required(ErrorMessage ="이름을 입력하시오.")]  //반드시 입력
+        [StringLength(25, MinimumLength =1, ErrorMessage ="이름은 1자 이상 25자 이하")] //길이 제한
+        public string Name { get; set; }
+        
+        [Display(Name = "내용: ")]
+        [Required(ErrorMessage = "내용을 입력하시오.")]
+        [StringLength(255, MinimumLength =1, ErrorMessage = "내용은 1자 이상 255자 이하")]
+        public string Content { get; set; }
+        
+    }
+}
