0d9f36724be8b48973921f92f77701e6377b862b
yyoungeun <1150amy@naver.com>
2020-04-09 11:05:01 +0900

    View Component

---
+++ Controllers/ViewComponentDemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class ViewComponentDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        ///<summary>
+        /// CopyrightViewComponent 출력 데모
+        /// </summary>
+        /// 
+        public IActionResult CopyrightDemo()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/CopyrightViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.ViewComponents
+{
+    ///<summary>
+    /// Copyright 뷰 컴포넌트
+    /// </summary>
+    public class CopyrightViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //초 단위로 짝수일 때와 홀수일 때 서로 다른 뷰 출력
+            string viewName = "Default";
+
+            if(DateTime.Now.Second % 2 == 0)
+            {
+                viewName = "Details";
+            }
+            return View(viewName);
+        }
+
+    }
+}
